a747109dece37549f412c287d72785432bbd9872
ArtARTs36 <temicska99@mail.ru>
2021-11-03 09:58:56 +0300

    resolve dependency

--- src/Providers/CurrencyProvider.php
+++ src/Providers/CurrencyProvider.php
@@ -4,17 +4,25 @@ namespace ArtARTs36\LaravelRuCurrency\Providers;
 
 use ArtARTs36\CbrCourseFinder\Contracts\Finder;
 use ArtARTs36\LaravelRuCurrency\Contracts\CurrencyRepository;
-use ArtARTs36\LaravelRuCurrency\Database\Seeders\RuCurrencySeeder;
 use ArtARTs36\LaravelRuCurrency\Ports\Console\Commands\FetchCoursesCommand;
 use ArtARTs36\LaravelRuCurrency\Repositories\EloquentCurrencyRepository;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
 class CurrencyProvider extends ServiceProvider
 {
     public function register()
     {
+        $this
+            ->app
+            ->when(\ArtARTs36\CbrCourseFinder\Finder::class)
+            ->needs(ClientInterface::class)
+            ->give(fn (Container $container) => $container->make(Client::class));
+
         $this->app->bind(Finder::class, \ArtARTs36\CbrCourseFinder\Finder::class);
-        $this->app->bind(CurrencyRepository::class, EloquentCurrencyRepository::class);;
+        $this->app->bind(CurrencyRepository::class, EloquentCurrencyRepository::class);
 
         if ($this->app->runningInConsole()) {
             $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
